b662ba1880bfab43b6ee61baef22ada8c12de351
jeremi-adrian <jeremipa92@gmail.com>
2021-11-29 18:53:10 -0600

    new

---
+++ assets/js/compra.js
@@ -0,0 +1,10 @@
+const compra = new Carrito();
+const listaCompra = document.querySelector('#lista-compra tbody');
+
+cargarEventos();
+
+function cargarEventos(){
+
+    document.addEventListener('DOMContentLoaded', compra.leerLocalStorageCompra());
+       
+}
